cdd75ca6d32bc4dacc07b729c5bed368aa506ddd
TinDT-b197 <tindt.24itb@vku.udn.vn>
2025-06-14 13:55:54 +0700

    WEB

--- public/js/home.js
+++ public/js/home.js
@@ -95,3 +95,15 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+        var MenuItems = document.getElementById("MenuItems");
+
+        MenuItems.style.maxHeight = "0px";
+        
+        function menutoggle() {
+            if(MenuItems.style.maxHeight =="0px") {
+                MenuItems.style.maxHeight = "200px";
+            }
+            else {
+                MenuItems.style.maxHeight = "0px";
+            }
+        }
